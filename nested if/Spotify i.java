e777e5b42e9bc19a0625d9aeaf54b2b51bce08da
Mk172029 <karnankannaww@gmail.com>
2025-12-28 10:06:07 +0530

    nested if
    NESTED IF

---
+++ nested if/Spotify i.java
@@ -0,0 +1,258 @@
+import java.util.Scanner;
+
+class Mukesh  
+{
+public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		{
+			System.out.println("SPOTIFY WELCOMES YOU");
+			System.out.println("A.R. RAHMAN.");
+			System.out.println("ANIRUDH");
+			System.out.println("HARISH JEYARAJ");
+			System.out.println("ILLAIYARAJA");
+			System.out.println("U1");
+			System.out.print("choose your options" );
+			
+			int choice = sc.nextInt();
+			
+		if(choice==1)
+		{	System.out.println("welcome A.R. RAHMAN songs");
+			System.out.println("1. Melody ");
+			System.out.println("2. Kuthu ");
+			System.out.println("3. Motivational Song ");
+			System.out.println("4. Sad ");
+			System.out.print("Select your songs :");
+			
+			int songs = sc.nextInt();
+			
+			if(songs==1)
+				
+				{
+					System.out.println("• Netru illadha Matram ");
+					System.out.println("• Enna Solla Pogirai ");
+					System.out.println("• Ennavale Adi Ennavale");
+					System.out.println("• Malargale Malargale");
+					
+				}
+			else if(songs==2)
+			{
+				System.out.println("Thaiya Thaiya ");
+				System.out.println("Mukkala Mukkabala");
+				System.out.println("Urvashi Urvashi");
+				System.out.println("Aathichudi ");
+			}
+			else if(songs==3)
+			{
+				System.out.println("Vande Mataram ");
+				System.out.println("Oruvan Oruvan mudhalli");
+				System.out.println("Shakthi kodu ");
+				System.out.println("Kappal Yeari Poyachu");
+			}
+			else if(songs==4)
+			{
+				System.out.println("Poongatrile");
+				System.out.println("Marudaani");
+				System.out.println("Minnalea En vazhvin");
+				System.out.println("Rasathi");
+				System.out.println("Oru poiyavathu Sol ");
+			}
+			
+		    else
+			{
+				System.out.println("songs not available");
+			}
+		}
+			else if(choice==2)
+		{	System.out.println("welcome ANIRUDH songs");
+			System.out.println("1. Melody ");
+			System.out.println("2. Kuthu ");
+			System.out.println("3. Motivational Song ");
+			System.out.println("4. Sad ");
+			System.out.print("Select your songs :");
+			
+			int songs = sc.nextInt();
+			
+			if(songs==2)
+				
+				{
+					System.out.println("• Neeyum Nanum Anbe  ");
+					System.out.println("• Velicha Poove ");
+					System.out.println("• Po Indru Neeyaga ");
+					System.out.println("• Nee partha vizhigal");
+					
+				}
+			else if(songs==3)
+			{
+				System.out.println("• Vaathi Raid");
+				System.out.println("• Naa Ready ");
+				System.out.println("• Badass");
+				System.out.println("• Vandha Edam ");
+			}
+			else if(songs==4)
+			{
+				System.out.println("Velai Illa pattathari ");
+				System.out.println("• Boomi Enna Suthudhe ");
+				System.out.println("• Ethir Neechal ");
+				System.out.println("• Surviva");
+			}
+			else if(songs==5)
+			{
+				System.out.println("• Porkanda Singam");
+				System.out.println("• Kanave Kanave");
+				System.out.println("• Kannaana Kanne ");
+				System.out.println("• Jodi Nilave");
+			}
+			
+		    else
+			{
+				System.out.println("songs not available");
+			}
+		}
+
+			else if(choice==3)
+		{	System.out.println("welcome HARISH JEYARAJ songs");
+			System.out.println("1. Melody ");
+			System.out.println("2. Kuthu ");
+			System.out.println("3. Motivational Song ");
+			System.out.println("4. Sad ");
+			System.out.print("Select your songs :");
+			
+			int songs = sc.nextInt();
+			
+			if(songs==3)
+				
+				{
+					System.out.println("• Unnale ");
+					System.out.println("• Annul Maele");
+					System.out.println("• Yedho Ondru ");
+					System.out.println("• Vaseegara ");
+					System.out.println("• Paartha Mudhal");
+				}
+			else if(songs==2)
+			{
+				System.out.println("• Tirunelveli Halwa Da ");
+				System.out.println("• Aradi katre");
+				System.out.println("• Soda Bottle ");
+				System.out.println("• Aruva Meesai ");
+			}
+			else if(songs==3)
+			{
+				System.out.println("• Venmathiye ");
+				System.out.println("• Oh maname  ");
+				System.out.println("• Yamma Yamma ");
+				System.out.println("• Othayile");
+			}
+			
+			
+		    else
+			{
+				System.out.println("songs not available");
+			}
+			
+		}
+		
+		else if(choice==4)
+		{	System.out.println("welcome ILLAIYARAJA songs");
+			System.out.println("1. Melody ");
+			System.out.println("2. Kuthu ");
+			System.out.println("3. Motivational Song ");
+			System.out.println("4. Sad ");
+			System.out.print("Select your songs :");
+			
+			int songs = sc.nextInt();
+			
+			if(songs==4)
+				
+				{
+					System.out.println("• Oru kili uruguthu ");
+					System.out.println("• Kuzhaloodhum kannanukku");
+					System.out.println("• Ennai thottu ");
+					System.out.println("• Raja Raja cholan");
+				}
+			else if(songs==2)
+			{
+				System.out.println("• Ooruvittu ooruvanthu ");
+				System.out.println("• Nooru varusham");
+				System.out.println("• Aasai Nooruvagai");
+				System.out.println("• Aasai Athigam vachu");
+			}
+			else if(songs==3)
+			{
+				System.out.println("• Kanne Kalaimane");
+				System.out.println("• Thene thenpaandi Neeye");
+				System.out.println("• Paadi Parantha Killi");
+				System.out.println("• Nilave Vaa");
+			}
+			
+		    else
+			{
+				System.out.println("songs not available");
+			}
+			
+		}
+		    else if(choice==4)
+		{	System.out.println("welcome U1 songs");
+			System.out.println("1. Melody ");
+			System.out.println("2. Kuthu ");
+			System.out.println("3. Motivational Song ");
+			System.out.println("4. Sad ");
+			System.out.print("Select your songs :");
+			
+			int songs = sc.nextInt();
+			
+			if(songs==4)
+				
+				{
+					System.out.println("• Aathadi Manasudhan ");
+					System.out.println("• Chinna sirisu ");
+					System.out.println("• Venmeggam ");
+					System.out.println("• Iayyayyo ");
+				}
+			else if(songs==2)
+			{
+				System.out.println("• Machi open the bottle");
+				System.out.println("• Saroja saman nikalo ");
+				System.out.println("• • No money no honey ");
+				System.out.println("• • Villaiyadu mankatha");
+			}
+			else if(songs==3)
+			{
+				System.out.println("• Edhirthu Nill ");
+				System.out.println("• Oru nalil ");
+				System.out.println("• Nimirnthu NIL");
+				System.out.println("• Ungakkule Mirugam ");
+			}
+			else if(songs==4)
+			{
+				System.out.println("• Kadhal valarthen ");
+				System.out.println("• Yedho ondru ennai");
+				System.out.println("• Pogadhe ");
+				System.out.println("• Ninaithu ninaithu ");
+			}
+			
+		    else
+			{
+				System.out.println("songs not available");
+			}
+			
+		}
+			
+			
+		
+			 else
+			{
+				System.out.println("songs not available");
+			}
+			
+			
+			
+			
+			
+			
+		
+
+		
+		}
+	}
+}
---
+++ nested if/TheWorldofKnowledge.java
@@ -0,0 +1,197 @@
+import java.util.Scanner;
+
+class TheWorldofKnowledge
+{
+public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		{
+			System.out.println("THE WORLD OF KNOWLEDGE");
+			System.out.println("FICTION ");
+			System.out.println("FANTASY ");
+			System.out.println("BUSINESS ");
+			System.out.println("PSYCHOLOGY");
+			System.out.print("choose your options:");
+			
+		int choice = sc.nextInt();
+		
+	 if(choice==1)
+		{
+			System.out.println(" Welcome FICTION");
+			System.out.println("1. RISE OF THE FALLEN: The Manwaan Series Book 1.");	
+			System.out.println("2. Shadows Rising ");	
+			System.out.println("3. Kunex, It Ends with Us ");
+			System.out.print("choose your RSK: ");
+			
+			
+		int Options = sc.nextInt();
+		
+	    if(Options==1)
+			{
+			System.out.println("Description:  A cultural fusion. A modern twist. An ancient battle of good and evil. Amidst an eternal family feud, a purposeless and hyper-anxious Rinad discovers that he is part human and part celestial  a Manwaan! As he navigates through his strange reality, he encounters magnificent Angels, along with savage Asuras and Rakshasas infiltrating Mumbai");
+			System.out.println("Author: Abhishek Krishnan");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.300");		
+            }	
+	    else if(Options==2)
+	 
+			{
+				System.out.println("Description: Banished from the celestial court for an unspeakable crime, Akran, once a powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author: Rohan Monteiro");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.250. ");
+			
+			
+			}
+		else if(Options==3)
+			{
+				System.out.println("Description:  The newest, highly anticipated court for an unspeakable crime, Akran, once a powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author: Rohan Monteiro");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.350. ");
+			
+			
+			}
+			 else
+			{
+				System.out.println("songs not available");
+			}
+		}
+			
+			
+		else if(choice==2)
+		{
+			System.out.println(" Welcome FANTASY ");
+			System.out.println("1. Compass and Blade");	
+			System.out.println("2. Blood metal Bone  ");	
+			System.out.println("3. The shadows between us. ");
+			System.out.print("choose your CBT: ");
+			
+			
+		int Options = sc.nextInt();
+			if(Options==2)
+			{
+			System.out.println("Description:  A cultural fusion. A modern twist. An ancient battle of good and evil. Amidst an eternal family feud, a purposeless and hyper-anxious Rinad discovers that he is part human and part celestial  a Manwaan! As he navigates through his strange reality, he encounters magnificent Angels, along with savage Asuras and Rakshasas infiltrating Mumbai");
+			System.out.println("Author:Rachel Greenlaw");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.370");		
+            }	
+	 else if(Options==2)
+	 
+			{
+				System.out.println("Description: Banished from the celestial court for an unspeakable crime, Akran, once a powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author: Lindsay Cummings");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.275");
+			
+			
+			}
+		else if(Options==3)
+			{
+				System.out.println("Description:  No one knows the extent of the freshly  crime, Akran, once a powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author: Tricia Leven-seller");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.304. ");
+			}
+	    else
+			{
+				System.out.println("songs not available");
+			}
+		}
+		
+		else if(choice==3)
+		{
+			System.out.println(" Welcome  BUSINESS");
+			System.out.println("1. RISE OF THE FALLEN: The Manwaan Series Book 1.");	
+			System.out.println("2. Shadows Rising ");	
+			System.out.println("3. Kunex, It Ends with Us ");
+			System.out.print("choose your RSK: ");
+		
+			
+		int Options = sc.nextInt();
+		
+	    if(Options==3)
+			{
+			System.out.println("Description:  A cultural fusion. A modern twist. An ancient battle of good and evil. Amidst an eternal family feud, a purposeless and hyper-anxious Rinad discovers that he is part human and part celestial  a Manwaan! As he navigates through his strange reality, he encounters magnificent Angels, along with savage Asuras and Rakshasas infiltrating Mumbai");
+			System.out.println("Author: Abhishek Krishnan");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.300");		
+            }	
+	    else if(Options==2)
+	 
+			{
+				System.out.println("Description: Banished from the celestial court for an unspeakable crime, Akran, once a powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author: Rohan Monteiro");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.250. ");
+			
+			
+			}
+		else if(Options==3)
+			{
+				System.out.println("Description:  The newest, highly anticipated court for an unspeakable crime, Akran, once a powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author: Rohan Monteiro");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.350. ");
+			
+			
+			}
+			 else
+			{
+				System.out.println("songs not available");
+			}
+		}
+		else if(choice==4)
+		{
+			System.out.println(" Welcome BUSINESS ");
+			System.out.println("1. The Psychology of Money");	
+			System.out.println("2. Do It Today ");	
+			System.out.println("3. Atomic Habits");
+			System.out.print("choose your RSK: ");
+		
+			
+		int Options = sc.nextInt();
+		
+	    if(Options==3)
+			{
+			System.out.println("Description:  Timeless lessons on wealth, greed,  An ancient battle of good and evil. Amidst an eternal family feud, a purposeless and hyper-anxious Rinad discovers that he is part human and part celestial  a Manwaan! As he navigates through his strange reality, he encounters magnificent Angels, along with savage Asuras and Rakshasas infiltrating Mumbai");
+			System.out.println("Author: Morgan Housel  ");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.320");		
+            }	
+	    else if(Options==2)
+	 
+			{
+				System.out.println("Description:  Are you also tired of putting off your dreams untila powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author:Darius Foroux");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.100");
+			
+			
+			}
+		else if(Options==3)
+			{
+				System.out.println("Description: People think that when you want to change your life,  crime, Akran, once a powerful yaksha, spends his days in the city of Mumbai, drinking cheap rum and pretending to be human. When a young girl is abducted, Akran finds himself reluctantly dragged into the investigation. But, as he soon discovers,  is not a simple  of kidnapping—villainous thugs, gods and demons are all involved. And an ancient evil is returning to the world.");
+			System.out.println("Author:: James Clear");
+			System.out.println("Language: English");
+			System.out.println("Rate: Rs.340. ");
+			
+			
+			}
+			 else
+			{
+				System.out.println("Book not available");
+			}
+		}
+			 else
+			{
+				System.out.println("world not available");
+			}
+			
+			
+
+			
+}
+	}			
+}	
+		
