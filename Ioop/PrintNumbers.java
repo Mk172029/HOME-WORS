086dce47576176f1f7c577f37bfbdc55f8c59eb3
Mk172029 <karnankannaww@gmail.com>
2025-12-31 11:20:43 +0530

    loop hw
    hw

---
+++ Ioop/PrintNumbers.java
@@ -0,0 +1,10 @@
+class PrintNumbers
+{
+	public static void main(String[] args)
+	{
+		for(int A = 1;A<=15;A++)
+		{
+			System.out.println(A);
+		}
+	}
+}	
---
+++ Ioop/Reverseorder.java
@@ -0,0 +1,10 @@
+class Reverseorder 
+{
+	public static void main(String[] args)
+	{
+		for(int A = 10; A>=1; A--)
+		{
+			System.out.println(A);
+		}
+	}
+}	
