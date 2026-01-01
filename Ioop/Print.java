fc59f7ca61625ad9fdc4b0e47c0923fb9a51b0fb
Mk172029 <karnankannaww@gmail.com>
2026-01-01 12:29:57 +0530

    hw
    hw

---
+++ Ioop/Print.java
@@ -0,0 +1,20 @@
+class Print
+{
+	
+	public static void main(String[] args)
+	{
+		int a = 1;
+		
+		while(a<=30)
+		{
+			if(a % 2 ==0)
+			
+			{
+			System.out.println(a +"is the even number");
+			
+			}
+			a++;
+		}
+
+	}
+}	
